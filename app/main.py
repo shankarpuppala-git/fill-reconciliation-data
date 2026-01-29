30eeb1f83c1d64823d805e724e5b9b997b81b7b9
root <root@AZ-ECOM-MT-QA-B-PFM-01.rm5vrvde5jzuhllwfypwklvlab.bx.internal.cloudapp.net>
2026-01-29 15:09:04 +0000

    env file is added and all the values are read from that

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.controller.reconciliation_controller import router
+from dotenv import load_dotenv
+load_dotenv()
 
 app = FastAPI(title="Reconciliation Service")
 
